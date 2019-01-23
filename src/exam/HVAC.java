7953d1982ea20fc04c4408bd4df202aec7aea113
ADRIAN NECHITA <nechitadi11@gmail.com>
2019-01-23 23:49:22 +0200

    Create getters and setters for room set temperature

--- src/exam/HVAC.java
+++ src/exam/HVAC.java
@@ -9,15 +9,23 @@ import java.util.List;
  */
 public class HVAC {
     
-    private static final int ROOM_SET_TEMPERATURE = 22;
-    private static final int ROOM_SET_HUMIDITY = 50;
+    private int roomSetTemperature = 22;
+    private int ROOM_SET_HUMIDITY = 50;
     String CELSIUS_SYMBOL = Character.toString((char) '\u00B0');
     private int readTemperature;    
     private int readHumidity;
+
+    public int getRoomSetTemperature() {
+        return roomSetTemperature;
+    }
+
+    public void setRoomSetTemperature(int roomSetTemperature) {
+        this.roomSetTemperature = roomSetTemperature;
+    }
     private List<Integer> temperatureHistory = new ArrayList();
     
     public void switchHeatingAndCooling(int currentValue) {
-        if (currentValue < ROOM_SET_TEMPERATURE) {
+        if (currentValue < roomSetTemperature) {
             heat(currentValue);
         } else {
             cool(currentValue);
@@ -26,20 +34,20 @@ public class HVAC {
     
     private void heat(Integer currentValue) {
         System.out.println("===A pornit incalzirea camerei===");
-        while(currentValue < ROOM_SET_TEMPERATURE) {
+        while(currentValue < roomSetTemperature) {
             currentValue++;         
             System.out.println("Temperatura a crescut la " + currentValue + CELSIUS_SYMBOL + "C");
         }
-        System.out.println("Temperatura a ajuns la nivelul setat: " + ROOM_SET_TEMPERATURE + CELSIUS_SYMBOL + "C");
+        System.out.println("Temperatura a ajuns la nivelul setat: " + roomSetTemperature + CELSIUS_SYMBOL + "C");
     }
     
     private void cool(Integer currentValue) {
         System.out.println("===A pornit sitemul de ventilatie===");
-        while(currentValue > ROOM_SET_TEMPERATURE) {
+        while(currentValue > roomSetTemperature) {
             currentValue--;         
             System.out.println("Temperatura a scazut la " + currentValue + CELSIUS_SYMBOL + "C");
         }
-        System.out.println("Temperatura a ajuns la nivelul setat: " + ROOM_SET_TEMPERATURE + CELSIUS_SYMBOL + "C");
+        System.out.println("Temperatura a ajuns la nivelul setat: " + roomSetTemperature + CELSIUS_SYMBOL + "C");
     }
     
     private void addToHistoryOfTemperatures() {
