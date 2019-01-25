e451981ef58ed4ee43d8c6958e1240d9a95b7bd6
ADRIAN NECHITA <nechitadi11@gmail.com>
2019-01-25 14:56:42 +0200

    Move create a method for each command

--- src/exam/HvacServer.java
+++ src/exam/HvacServer.java
@@ -27,6 +27,7 @@ public class HvacServer {
 class SensorHandler extends Thread{
     Socket s;
     HVAC hvac = new HVAC();
+    String responseFromHVAC;
     boolean active = true;
     
  
@@ -41,7 +42,7 @@ class SensorHandler extends Thread{
             
             while (active) {
                 String line = in.readLine(); //asteapta mesaje de la client
-                System.out.println("\nAm citit valoare de la senzor: \n" + line);
+                System.out.println("\n=== Citire de la senzor ===\n" + line);
                 out.println(performActions(line));       
             }
  
@@ -51,66 +52,83 @@ class SensorHandler extends Thread{
     }
     
     public String performActions(String line) throws IOException {
-        System.out.println("\n===Afisez date==");
-        String responseFromHVAC;
+        System.out.println("\n=== Afisez date ==");
         
         if (line.contains("T: ")) {
-            String typedValue = line.replace("T: ", "");
-            Integer temperatureFromSensor = Integer.parseInt(typedValue);
-            hvac.setReadTemperature(temperatureFromSensor);
-            responseFromHVAC = hvac.switchHeatingAndCooling(temperatureFromSensor);
-            return responseFromHVAC;
+            return readTemperatureAndProcess(line);
         } else if (line.contains("SetT ")) {
-            String typedValue = line.replace("SetT ", "");
-            Integer roomSetTemperature = Integer.parseInt(typedValue);
-            hvac.setRoomSetTemperature(roomSetTemperature);
-            responseFromHVAC = "Temperatura a fost setata la: ";
-            responseFromHVAC += hvac.getRoomSetTemperature() + hvac.CELSIUS_SYMBOL + "C";
-            responseFromHVAC += "\nDONE";
-
-            return responseFromHVAC;
+            return setTemperature(line);
         } else if (line.contains("H: ")) {
-            String typedValue = line.replace("H: ", "");
-            Integer humidityFromSensor = Integer.parseInt(typedValue);
-            hvac.setReadHumidity(humidityFromSensor);
-            responseFromHVAC = hvac.switchHumidifierDevice(humidityFromSensor);
-            return responseFromHVAC;
+            return readHumidityAndProcess(line);
         } else if (line.contains("SetH ")) {
-            String typedValue = line.replace("SetH ", "");
-            Integer roomSetHumidity = Integer.parseInt(typedValue);
-            hvac.setRoomSetHumidity(roomSetHumidity);
-            responseFromHVAC = "Humidatea dorita a fost setata la: ";
-            responseFromHVAC += hvac.getRoomSetHumidity() + "%";
-            responseFromHVAC += "\nDONE";
-
-            return responseFromHVAC;
+            return setHumidity(line);
         } else if (line.contains("Inchide")) {
             System.out.println("Conexiunea senzorului a fost intrerupta!");
             s.close();
             active = false;
         } else if (line.contains("Grafic")) {
-            String typedValues = line.replace("Grafic", "");
-            
-            StringTokenizer st = new StringTokenizer(typedValues, " ");               
-            String graphType = st.nextToken();
-            Integer graphScale = Integer.parseInt(st.nextToken());
-            
-            System.out.println("GR TYPE: " + graphType);
-            System.out.println("GR SCLAE: " + graphScale);
-
-            if (graphType.equals("T")) {
-                Graph grafic = new Graph(hvac.temperatureHistory, graphType, graphScale);
-            } else if (graphType.equals("H")) {
-                Graph grafic = new Graph(hvac.humidityHistory, graphType, graphScale);
-            }
-
-            return "\nDONE";
-        }
-        
-        else {
+            return drawGraph(line);
+        } else {
             return "Comandata inexistenta.\nDONE";
         }
+        
         return null;
+    }
+    
+    private String readTemperatureAndProcess(String line) {
+        String typedValue = line.replace("T: ", "");
+        Integer temperatureFromSensor = Integer.parseInt(typedValue);
+        hvac.setReadTemperature(temperatureFromSensor);
+        responseFromHVAC = hvac.switchHeatingAndCooling(temperatureFromSensor);
+        
+        return responseFromHVAC;
+    }
+    
+    private String setTemperature(String line) {
+        String typedValue = line.replace("SetT ", "");
+        Integer roomSetTemperature = Integer.parseInt(typedValue);
+        hvac.setRoomSetTemperature(roomSetTemperature);
+        responseFromHVAC = "Temperatura a fost setata la: ";
+        responseFromHVAC += hvac.getRoomSetTemperature() + hvac.CELSIUS_SYMBOL + "C";
+        responseFromHVAC += "\nDONE";
+
+        return responseFromHVAC;
+    }
+    
+    private String readHumidityAndProcess(String line) {
+        String typedValue = line.replace("H: ", "");
+        Integer humidityFromSensor = Integer.parseInt(typedValue);
+        hvac.setReadHumidity(humidityFromSensor);
+        responseFromHVAC = hvac.switchHumidifierDevice(humidityFromSensor);
+        
+        return responseFromHVAC;
+    }
+    
+    private String setHumidity(String line) {
+        String typedValue = line.replace("SetH ", "");
+        Integer roomSetHumidity = Integer.parseInt(typedValue);
+        hvac.setRoomSetHumidity(roomSetHumidity);
+        responseFromHVAC = "Humidatea dorita a fost setata la: ";
+        responseFromHVAC += hvac.getRoomSetHumidity() + "%";
+        responseFromHVAC += "\nDONE";
+
+        return responseFromHVAC;
+    }
+    
+    private String drawGraph(String line) {
+        String typedValues = line.replace("Grafic", "");
+            
+        StringTokenizer st = new StringTokenizer(typedValues, " ");               
+        String graphType = st.nextToken();
+        Integer graphScale = Integer.parseInt(st.nextToken());
+
+        if (graphType.equals("T")) {
+            Graph grafic = new Graph(hvac.temperatureHistory, graphType, graphScale);
+        } else if (graphType.equals("H")) {
+            Graph grafic = new Graph(hvac.humidityHistory, graphType, graphScale);
+        }
+
+        return "Graficul a fost realizat.\nDONE";
     }
     
 }
